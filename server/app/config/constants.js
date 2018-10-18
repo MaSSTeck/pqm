6bf2897df095b8883e2d2ec26d68ec07b2cb8575
Seunope <seunope2004@yahoo.co.uk>
2018-10-18 10:09:31 +0100

    pull out server backend from project

--- server/app/config/constants.js
+++
@@ -1,2 +0,0 @@
-export const PROD_API  = "https://questions.aloc.ng/api/";
-export const DEV_API  = "http://localhost/aloc-o/public/api/";
--- src/config/constants.js
+++ src/config/constants.js
@@ -1,2 +1,2 @@
-export const PROD_API  = "http://localhost:4000";
+export const PROD_API  = "https://questions.aloc.ng/api/";
 export const DEV_API  = "http://localhost:4000";
