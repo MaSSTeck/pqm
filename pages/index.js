a6164c4c3b502ce8f6494075e7e8f6cb0702db8a
Seunope <seunope2004@yahoo.co.uk>
2018-11-05 10:41:20 +0100

    added metatag

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,10 @@ import FooterPQM from '../src/layout/FooterFastQ';
 export default withData(props => (
   <div>
     <Head>
+      
         <title>Fast | Question </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
+
     </Head>
 
     <Visibility>
--- src/layout/HeaderFastQ.js
+++ src/layout/HeaderFastQ.js
@@ -10,6 +10,7 @@ const HeaderFastQ =  (props)=> {
         <div>
             <Head>
                 <title>FQ | {titleText}</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"></meta>
             </Head>
             <MenuBar/>
         </div>
